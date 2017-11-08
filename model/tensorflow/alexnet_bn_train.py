c4eb8e5f51b5ed65b1f5aca0dfd2537e8c0ad36e
Ubuntu <ubuntu@ip-172-31-87-252.ec2.internal>
2017-11-08 20:08:04 +0000

    Changed path of saved models for alex_bn, added alexnet_bn folder for saved models

--- model/tensorflow/alexnet_bn_train.py
+++ model/tensorflow/alexnet_bn_train.py
@@ -15,9 +15,9 @@ data_mean = np.asarray([0.45834960097,0.44674252445,0.41352266842])
 learning_rate = 0.001
 dropout = 0.5 # Dropout, probability to keep units
 training_iters = 50000
-step_display = 50
-step_save = 1000
-path_save = 'alexnet_bn'
+step_display = 25
+step_save = 500
+path_save = './alexnet_bn/alexnet_bn'
 start_from = ''
 
 def batch_norm_layer(x, train_phase, scope_bn):
