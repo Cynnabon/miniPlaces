fa546594076401b54b7d1cce9208c9cfbc551cdf
Cynthia Zhou <cyntzhou@gmail.com>
2017-11-14 11:29:13 -0500

    lala2

--- model/tensorflow/zfnet_bn_train.py
+++ model/tensorflow/zfnet_bn_train.py
@@ -66,50 +66,50 @@ def zfnet(x, keep_dropout, train_phase):
     # conv1 = tf.nn.conv2d(conv1, weights['wc1.5'], strides=[1, 2, 2, 1], padding="SAME")
     # print(conv1.get_shape())
 #    conv1 = tf.add(conv1, biases['conv1'])
-    conv1 = batch_norm_layer(conv1, train_phase, 'bn1')
-    conv1 = tf.nn.relu(conv1)
+    # conv1 = batch_norm_layer(conv1, train_phase, 'bn1')
+    # conv1 = tf.nn.relu(conv1)
     pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
     # pool1 = tf.nn.max_pool(pool1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
 
     # Conv + ReLU  + Pool, 27-> 13
     # 55 -> 26 -> 13
     conv2 = tf.nn.conv2d(pool1, weights['wc2'], strides=[1, 2, 2, 1], padding='SAME')  # 1 -> 2 stride
-    conv2 = batch_norm_layer(conv2, train_phase, 'bn2')
-    conv2 = tf.nn.relu(conv2)
+    # conv2 = batch_norm_layer(conv2, train_phase, 'bn2')
+    # conv2 = tf.nn.relu(conv2)
     pool2 = tf.nn.max_pool(conv2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
 
     # Conv + ReLU, 13-> 13
     conv3 = tf.nn.conv2d(pool2, weights['wc3'], strides=[1, 1, 1, 1], padding='SAME')
-    conv3 = batch_norm_layer(conv3, train_phase, 'bn3')
-    conv3 = tf.nn.relu(conv3)
+    # conv3 = batch_norm_layer(conv3, train_phase, 'bn3')
+    # conv3 = tf.nn.relu(conv3)
 
     # Conv + ReLU, 13-> 13
     conv4 = tf.nn.conv2d(conv3, weights['wc4'], strides=[1, 1, 1, 1], padding='SAME')
-    conv4 = batch_norm_layer(conv4, train_phase, 'bn4')
-    conv4 = tf.nn.relu(conv4)
+    # conv4 = batch_norm_layer(conv4, train_phase, 'bn4')
+    # conv4 = tf.nn.relu(conv4)
 
     # Conv + ReLU + Pool, 13->6
     conv5 = tf.nn.conv2d(conv4, weights['wc5'], strides=[1, 1, 1, 1], padding='SAME')
-    conv5 = batch_norm_layer(conv5, train_phase, 'bn5')
-    conv5 = tf.nn.relu(conv5)
+    # conv5 = batch_norm_layer(conv5, train_phase, 'bn5')
+    # conv5 = tf.nn.relu(conv5)
     pool5 = tf.nn.max_pool(conv5, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
 
     # FC + ReLU + Dropout
     fc6 = tf.reshape(pool5, [-1, weights['wf6'].get_shape().as_list()[0]])
     fc6 = tf.matmul(fc6, weights['wf6'])
-    fc6 = batch_norm_layer(fc6, train_phase, 'bn6')
-    fc6 = tf.nn.relu(fc6)
-    fc6 = tf.nn.dropout(fc6, keep_dropout)
+    # fc6 = batch_norm_layer(fc6, train_phase, 'bn6')
+    # fc6 = tf.nn.relu(fc6)
+    # fc6 = tf.nn.dropout(fc6, keep_dropout)
 
     # FC + ReLU + Dropout
     fc7 = tf.matmul(fc6, weights['wf7'])
     fc7 = batch_norm_layer(fc7, train_phase, 'bn7')
-    fc7 = tf.nn.relu(fc7)
-    fc7 = tf.nn.dropout(fc7, keep_dropout)
+    # fc7 = tf.nn.relu(fc7)
+    # fc7 = tf.nn.dropout(fc7, keep_dropout)
 
     # Output FC
     out = tf.add(tf.matmul(fc7, weights['wo']), biases['bo'])
-    print(out.get_shape())
+    
     return out
 
 # Construct dataloader
@@ -145,7 +145,7 @@ train_phase = tf.placeholder(tf.bool)
 
 # Construct model
 logits = zfnet(x, keep_dropout, train_phase)
-print(logits.get_shape())
+
 # Define loss and optimizer
 loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits))
 train_optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
