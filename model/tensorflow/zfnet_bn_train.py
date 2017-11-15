f46718999a6f8c2ca3c257ef4db465c807ab28f1
Ubuntu <ubuntu@ip-172-31-27-104.ec2.internal>
2017-11-15 23:35:28 +0000

    ZFNet bn train results

--- model/tensorflow/zfnet_bn_train.py
+++ model/tensorflow/zfnet_bn_train.py
@@ -18,7 +18,7 @@ training_iters = 50000
 step_display = 50
 step_save = 1000
 path_save = './zfnet_bn/zfnet_bn_dropout_0.5'
-start_from = ''
+start_from = './zfnet_bn/zfnet_bn_dropout_0.5-15000'
 
 def batch_norm_layer(x, train_phase, scope_bn):
     return batch_norm(x, decay=0.9, center=True, scale=True,
