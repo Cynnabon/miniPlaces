761771479f61cec5b79d71a601de26e5a9a57b2f
Ubuntu <ubuntu@ip-172-31-30-3.ec2.internal>
2017-11-16 17:13:36 +0000

    VGGNet 0.00001 learning rate results

--- model/tensorflow/vggnet_train.py
+++ model/tensorflow/vggnet_train.py
@@ -16,9 +16,9 @@ learning_rate = 0.00001
 dropout = 0.5 # Dropout, probability to keep units
 training_iters = 50000
 step_display = 50
-step_save = 1000
+step_save = 5000
 path_save = './vggnet/vggnet_0.00001'
-start_from = ''
+start_from = './vggnet/vggnet_0.00001-15000'
 
 def batch_norm_layer(x, train_phase, scope_bn):
     return batch_norm(x, decay=0.9, center=True, scale=True,
